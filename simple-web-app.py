021e014a538e660d09704a777d0be2b9f357aefc
PRABHU N <69341981+prabhun2001@users.noreply.github.com>
2023-02-15 12:00:37 +0530

    Update simple-web-app.py

--- simple-web-app.py
+++ simple-web-app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 def index():
         return "Congratulations, it's a web app!"
 if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=8080)
+        app.run(host="0.0.0.0", port=6666)
